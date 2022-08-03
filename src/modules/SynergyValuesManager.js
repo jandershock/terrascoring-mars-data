9f263dcbd0d98bc138baa8ee5704ae6c0008fea8
jandershock <jordan.andershock@gmail.com>
2022-08-03 11:30:29 -0500

    Refactored for server use

--- src/modules/SynergyValuesManager.js
+++
@@ -1,6 +0,0 @@
-import { remoteURL } from "../settings"
-
-export const getSynergyValuesByFirstId = (id) => {
-    return fetch(`${remoteURL}/synergy_values?firstId=${id}`)
-        .then(response => response.json())
-}
